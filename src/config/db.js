041bd278bbfcafe0d092d72fdf68e5a794216b1e
Pedro Silva <pedrosilva.prodev@gmail.com>
2025-10-05 15:40:30 -0300

    Conecta ao banco

---
+++ src/config/db.js
@@ -0,0 +1,34 @@
+import mssql from "mssql";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve('../.env') });
+
+
+const sql = mssql;
+
+const config = {
+  user: process.env.DB_USER?.trim(),
+  password: process.env.DB_PASS?.trim(),
+  server: process.env.DB_SERVER?.trim(),
+  database: process.env.DB_DATABASE?.trim(),
+  port: parseInt(process.env.DB_PORT) || 1433,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+
+console.log("Config do MSSQL:", config); // 🔹 debug
+
+let pool;
+
+async function connectDB() {
+  if (!pool) {
+    pool = await sql.connect(config);
+    console.log("✅ Conectado ao banco de dados!");
+  }
+  return pool;
+}
+
+export { sql, connectDB };
---
+++ src/server.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+
+import app from "./app.js";
+import { connectDB } from "./config/db.js";
+
+const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Erro ao conectar ao banco:", err);
+  }
+}
+
+startServer();
